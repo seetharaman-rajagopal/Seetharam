83293891493630ce351841072eb278b0e3a9705b
Seetharaman <seetharaman.chn@gmail.com>
2022-04-09 10:03:29 +0530

    double pointer

--- Seetharam/main.cpp
+++ Seetharam/main.cpp
@@ -4,59 +4,34 @@
 #include <vector>
 #include <time.h>
 #include <stdlib.h>
+#include<iomanip>
 
 using namespace std;
 
-void printme(int arr[], int size)
-{
-    for(int index =0; index < size;index++)
-    {
-        cout << arr[index] << " ";
-    }
-    cout << endl;
-}
 
-//template<typename  T>
-//void swapme(T& a, T &b)
-//{
-//    auto temp = a;
-//    a = b;
-//    b = temp;
-//}
 
-void bubblesort(int arr[], int nsize)
-{
-    auto swapme = [](int& a, int& b)
-    {
-        auto temp = a;
-        a = b;
-        b = temp;
-    };
-    for (int index =0; index < nsize;index++)
-    {
-        for(int subindex = 0; subindex < nsize -(1 + index) ; subindex++)
-        {
-            if(arr[subindex] > arr[subindex+1])
-            {
-                swapme(arr[subindex], arr[subindex+1]);
-            }
-        }
-    }
-}
 
 int main()
 {
-    int arr[5] {};
-    int narraysize = sizeof(arr)/sizeof(int);
-    auto var = time(NULL);
-    srand(var);
-    for(int index = 0; index < narraysize; index++)
+    int ** p = nullptr;
+    
+    p = new int*[3];
+    
+    p[0] =  new int [4];
+    p[1] = new int [4];
+    p[2] = new int[4];
+    
+    for(int index =0;index< 3;index++)
     {
-        arr[index] = rand()%100;
+        for(int subindex =0; subindex < 4;subindex++)
+        {
+            p[index][subindex] = index* 10 + subindex;
+        }
     }
-    cout << "array size is " << narraysize << endl;
-    printme(arr,narraysize);
-    bubblesort(arr,narraysize);
-    printme(arr,narraysize);
-    cout << endl;
+    
+    cout << p[1][2] << endl;
+    cout << *(*(p+1)+2) << endl;
+    
+    
+    return 0;
 }

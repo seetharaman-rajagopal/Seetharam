412ba0968ba345b1d1ae97228d4d55470a02148c
Seetharaman <seetharaman.chn@gmail.com>
2022-04-10 22:19:25 +0530

    copy and move

--- Seetharam/main.cpp
+++ Seetharam/main.cpp
@@ -9,28 +9,85 @@
 using namespace std;
 
 
+class Sample
+{
+public:
+    int m_size;
+    int * arr;
+public:
+    Sample(int psize =5):m_size(psize),arr(nullptr)
+    {
+        cout << "constructior is called" << endl;
+        if(arr == nullptr)
+        {
+            arr = new int[m_size]{};
+        }
+        
+    }
+    Sample(const Sample& rsample)
+    {
+        cout << "copy constructor is called " << endl;
+        if(this != &rsample)
+        {
+            m_size = rsample.m_size;
+            arr = new int[m_size];
+            for(int index= 0;index< m_size;index++)
+            {
+                arr[index] = rsample.arr[index];
+            }
+        }
+    }
+    
+    Sample(Sample&& rsample)
+    {
+        cout << "move constructor is called" << endl;
+        if(this != &rsample)
+        {
+            m_size = rsample.m_size;
+            arr = rsample.arr;
+            rsample.arr = nullptr;
+        }
+        
+    }
+    
+    void print()
+    {
+        for(int index =0;index < m_size; index++)
+        {
+            std::cout << arr[index] << endl;
+        }
+    }
+    
+    ~Sample()
+    {
+        delete [] arr;
+        arr = nullptr;
+    }
+};
+
+Sample createSample()
+{
+    Sample sample(5);
+    for(int index = 0;index < sample.m_size;index++)
+    {
+        sample.arr[index] = index*10;
+    }
+    return sample;
+}
+
+void usesample(Sample sample)
+{
+    sample.print();
+}
 
 
 int main()
 {
-    int ** p = nullptr;
+    Sample sample = createSample();
+    usesample(sample);
     
-    p = new int*[3];
+    usesample(std::move(createSample()));
     
-    p[0] =  new int [4];
-    p[1] = new int [4];
-    p[2] = new int[4];
-    
-    for(int index =0;index< 3;index++)
-    {
-        for(int subindex =0; subindex < 4;subindex++)
-        {
-            p[index][subindex] = index* 10 + subindex;
-        }
-    }
-    
-    cout << p[1][2] << endl;
-    cout << *(*(p+1)+2) << endl;
     
     
     return 0;

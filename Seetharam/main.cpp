7f58f8e0eadbc6a95eea12d432fd0cc160c4ae0d
Seetharaman <seetharaman.chn@gmail.com>
2022-05-01 16:01:47 +0530

    Ransom Note

--- Seetharam/main.cpp
+++ Seetharam/main.cpp
@@ -6,115 +6,46 @@
 #include <stdlib.h>
 #include<iomanip>
 #include<map>
+#include <stack>
+#include<unordered_map>
 
 using namespace std;
 
-//https://www.youtube.com/watch?v=QN9hnmAgmOc&t=139s
- 
-#include<iostream>
+#include<string>
 
 using namespace std;
 
 class Solution {
 public:
-    
-    int getvalue(char letter)
+    bool canConstruct(string ransomNote, string magazine)
     {
-        switch(letter)
+        std::unordered_map<char, int> letters_count;
+        for(auto letter:magazine)
         {
-            case 'I':
-                {
-                    return 1;
-                }
-            case 'V':
-                {
-                    return 5;
-                }
-            case 'X':
-                {
-                    return 10;
-                }
-            case 'L':
-                {
-                    return 50;
-                }
-            case 'C':
-                {
-                    return 100;
-                }
-            case 'D':
-                {
-                    return 500;
-                }
-            case 'M':
-                {
-                    return 1000;
-                }
-            default:
-                return 0;
-            
+            ++letters_count[letter];
         }
-    }
-    
-    
-//    int romanToInt(string s)
-//    {
-//        int out{};
-//        char current_letter{};
-//        char next_letter{};
-//        int next{}, current{};
-//        for(int index =0;  index < s.length();index++)
-//        {
-//            current_letter = s[index];
-//            next_letter = s[index+1];
-//            current = getvalue(current_letter);
-//            next = getvalue(next_letter);
-//            if((current >= next))
-//            {
-//                out = out + current;
-//            }
-//            else
-//            {
-//                out = out + next -current;
-//                index = index +1;
-//            }
-//        }
-//        return out;
-//    }
-    
-    int romanToInt(string s) {
-            map<char, int> values;
-            values['I'] = 1;
-            values['V'] = 5;
-            values['X'] = 10;
-            values['L'] = 50;
-            values['C'] = 100;
-            values['D'] = 500;
-            values['M'] = 1000;
-            
-            int last = s.length()-1;
-            int ans = values[s[last]];
-            
-            for(int i = last-1; i>= 0; i--)
+        
+        for(auto eachchar:ransomNote)
+        {
+            if(letters_count[eachchar] == 0)
             {
-                if(values[s[i]] < values[s[i+1]])
-                {
-                    ans-= values[s[i]];
-                }
-                else
-                {
-                    ans+= values[s[i]];
-                }
+                return false;
             }
-            
-            return ans;
+            else
+            {
+                --letters_count[eachchar];
+            }
         }
+        
+        return true;
+    }
 };
+
  
 // Driver Code
 int main()
 {
     Solution sol;
-    cout << sol.romanToInt("MCMXCIV") << endl;;
+    cout << sol.canConstruct("aaaa","malayalam") << endl;;
 
 }
